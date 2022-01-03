e0f622deba2dc6028d4bebaf2c7c53936d2797dd
arsgsg1 <arsgsg1@gmail.com>
2022-01-03 16:54:54 +0900

    profile controller 추가 및 local yml설정 변경

---
+++ src/main/java/com/ko/mediate/HC/controller/ProfileController.java
@@ -0,0 +1,22 @@
+package com.ko.mediate.HC.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+  private final Environment env;
+
+  @GetMapping("/profile")
+  public String profile() {
+    List<String> profiles = Arrays.asList(env.getActiveProfiles());
+    List<String> realProfiles = Arrays.asList("prod", "prod1", "prod2");
+    String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+    return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+  }
+}
