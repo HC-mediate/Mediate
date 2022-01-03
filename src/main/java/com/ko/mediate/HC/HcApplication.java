332ec2aff8261ef468096f3fb79398b6fc61136d
arsgsg1 <arsgsg1@gmail.com>
2022-01-03 12:56:25 +0900

    init project

---
+++ src/main/java/com/ko/mediate/HC/HcApplication.java
@@ -0,0 +1,13 @@
+package com.ko.mediate.HC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ko/mediate/HC/HcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ko.mediate.HC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
