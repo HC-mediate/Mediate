90bd0e09f8c434c00f766cc1a6a15c9200aae136
arsgsg1 <arsgsg1@gmail.com>
2022-01-10 15:13:35 +0900

    오타수정 22

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source $(ABSDIR)/profile.sh
+source ${ABSDIR}/profile.sh
 
-function switch_proxy(){
+function switch_proxy() {
     IDLE_PORT=$(find_idle_port)
 
-    echo "> 전환할Port: $IDLE_PORT"
+    echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
