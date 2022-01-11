70d065542d29d22941eb9edf83e14a1f89ff76a0
arsgsg1 <arsgsg1@gmail.com>
2022-01-11 11:59:17 +0900

    전화번호 정규식 수정

--- src/main/java/com/ko/mediate/HC/account/application/dto/request/SignupDto.java
+++ src/main/java/com/ko/mediate/HC/account/application/dto/request/SignupDto.java
@@ -17,7 +17,8 @@ public class SignupDto {
   @NotEmpty
   @Size(min = 3, max = 100, message = "비밀번호는 최소 3자이상, 100자이하여야 합니다.")
   private String password;
+
   @NotEmpty
-  @Pattern(regexp="(^$|[0-9]{10})", message = "유효하지 않은 전화번호입니다.")
+  @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "유효하지 않은 전화번호입니다.")
   private String phoneNum;
 }
