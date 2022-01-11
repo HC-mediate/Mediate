3adeb285bce590224dbcc27b4fab059d75bad7a6
arsgsg1 <arsgsg1@gmail.com>
2022-01-11 15:06:14 +0900

    로그인 테스트 추가

---
+++ src/test/java/com/ko/mediate/HC/AuthTest/AuthTest.java
@@ -0,0 +1,68 @@
+package com.ko.mediate.HC.AuthTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ko.mediate.HC.account.application.AccountRepository;
+import com.ko.mediate.HC.account.application.dto.request.LoginDto;
+import com.ko.mediate.HC.account.domain.Account;
+import java.util.Arrays;
+import java.util.HashSet;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("local")
+public class AuthTest {
+
+  @Autowired private PasswordEncoder passwordEncoder;
+  @Autowired private AccountRepository accountRepository;
+  @Autowired private MockMvc mvc;
+  @Autowired ObjectMapper objectMapper;
+  private static String password;
+  private static int order = 1; // 매 시도마다 아이디를 다르게 하기 위함.
+
+  @BeforeEach
+  public void setup() {
+    password = passwordEncoder.encode("test");
+    Account account =
+        new Account("test" + ++order, password, "010-1234-5678", new HashSet<>(Arrays.asList("USER")));
+    accountRepository.save(account);
+  }
+
+  @Test
+  @DisplayName("올바른 로그인 시도")
+  public void validAuth() throws Exception {
+    String id = "test" + order;
+    String params = objectMapper.writeValueAsString(new LoginDto(id, "test"));
+    mvc.perform(post("/api/auth").contentType(MediaType.APPLICATION_JSON).content(params))
+        .andExpect(status().isOk())
+        .andDo(print());
+  }
+
+  @Test
+  @DisplayName("아이디, 비번 틀린 시도")
+  public void invalidAuth() throws Exception {
+    String id = "test" + order;
+    String params = objectMapper.writeValueAsString(new LoginDto(id, "1234"));
+    mvc.perform(post("/api/auth").contentType(MediaType.APPLICATION_JSON).content(params))
+        .andExpect(status().isBadRequest())
+        .andDo(print());
+    id = "test1" + order;
+    params = objectMapper.writeValueAsString(new LoginDto(id, "1234"));
+    mvc.perform(post("/api/auth").contentType(MediaType.APPLICATION_JSON).content(params))
+        .andExpect(status().isBadRequest())
+        .andDo(print());
+  }
+}
