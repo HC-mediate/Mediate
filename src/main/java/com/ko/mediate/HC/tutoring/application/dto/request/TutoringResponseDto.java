92c1b39b13a30dcc2ab91fd44ce0846b0fe8f4c2
arsgsg1 <arsgsg1@gmail.com>
2022-01-28 14:00:56 +0900

    토큰 유효시간 8분으로 늘림, enum타입 not empty validation 제거

--- src/main/java/com/ko/mediate/HC/tutoring/application/dto/request/TutoringResponseDto.java
+++ src/main/java/com/ko/mediate/HC/tutoring/application/dto/request/TutoringResponseDto.java
@@ -16,10 +16,8 @@ public class TutoringResponseDto {
   private String toAccountId;
 
   @ApiModelProperty(value = "튜터/튜티 타입")
-  @NotEmpty(message = "타입은 반드시 있어야 합니다.")
   private RoleType type;
 
   @ApiModelProperty(value = "응답 유형 (수락/거절)")
-  @NotEmpty(message = "응답은 반드시 있어야 합니다.")
   private TutoringResponseType responseType;
 }
