819791623872c322ed7071bdbf5e6b427c1796d9
arsgsg1 <arsgsg1@gmail.com>
2022-02-14 14:56:54 +0900

    feat: firebase 알림 설정 서비스 및 메시지 추가

---
+++ src/main/java/com/ko/mediate/HC/notification/FcmMessage.java
@@ -0,0 +1,34 @@
+package com.ko.mediate.HC.notification;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class FcmMessage {
+
+  @JsonProperty(value = "validate_only")
+  private boolean validateOnly;
+
+  private Message message;
+
+  @Builder
+  @AllArgsConstructor
+  @Getter
+  public static class Message {
+    private Notification notification;
+    private String token;
+  }
+
+  @Builder
+  @AllArgsConstructor
+  @Getter
+  public static class Notification {
+    private String title;
+    private String body;
+    private String image;
+  }
+}
---
+++ src/main/java/com/ko/mediate/HC/notification/infra/FirebaseCloudMessageService.java
@@ -0,0 +1,78 @@
+package com.ko.mediate.HC.notification.infra;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.ko.mediate.HC.notification.FcmMessage;
+import java.io.IOException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FirebaseCloudMessageService {
+  private final Logger logger = LoggerFactory.getLogger(FirebaseCloudMessageService.class);
+
+  @Value("${firebase.config-path}")
+  private String firebaseConfigPath;
+
+  @Value("${firebase.api-url}")
+  private String apiUrl;
+
+  private final ObjectMapper objectMapper;
+
+  public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+    String message = makeMessage(targetToken, title, body);
+
+    OkHttpClient client = new OkHttpClient();
+    RequestBody requestBody =
+        RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+    Request request =
+        new Request.Builder()
+            .url(apiUrl)
+            .post(requestBody)
+            .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+            .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+            .build();
+    Response response = client.newCall(request).execute();
+    logger.info(response.body().string());
+  }
+
+  public String makeMessage(String targetToken, String title, String body)
+      throws JsonProcessingException {
+    FcmMessage fcmMessage =
+        FcmMessage.builder()
+            .message(
+                FcmMessage.Message.builder()
+                    .token(targetToken)
+                    .notification(
+                        FcmMessage.Notification.builder()
+                            .title(title)
+                            .body(body)
+                            .image(null)
+                            .build())
+                    .build())
+            .validateOnly(false)
+            .build();
+    return objectMapper.writeValueAsString(fcmMessage);
+  }
+
+  private String getAccessToken() throws IOException {
+    GoogleCredentials googleCredentials =
+        GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+            .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+    googleCredentials.refreshIfExpired();
+    return googleCredentials.getAccessToken().getTokenValue();
+  }
+}
