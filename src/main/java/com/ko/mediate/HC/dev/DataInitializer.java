f8d820c90ae3ea78c97f89ec6c26641300d7cc9d
arsgsg1 <arsgsg1@gmail.com>
2022-02-21 18:09:36 +0900

    테스트 더미 데이터에도 좌표 값 적용

--- src/main/java/com/ko/mediate/HC/dev/DataInitializer.java
+++ src/main/java/com/ko/mediate/HC/dev/DataInitializer.java
@@ -1,6 +1,7 @@
 package com.ko.mediate.HC.dev;
 
 import com.ko.mediate.HC.auth.domain.Account;
+import com.ko.mediate.HC.common.Coordinate;
 import com.ko.mediate.HC.tutee.Infra.JpaTuteeRepository;
 import com.ko.mediate.HC.tutee.domain.Tutee;
 import com.ko.mediate.HC.tutor.Infra.JpaTutorRepository;
@@ -12,6 +13,7 @@ import com.ko.mediate.HC.tutoring.domain.Tutoring;
 import com.ko.mediate.HC.tutoring.infra.JpaAccountRepository;
 import com.ko.mediate.HC.tutoring.infra.JpaTutoringRepository;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.io.ParseException;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
@@ -20,7 +22,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Profile(value = {"local"})
+@Profile(value = {"local", "local-maria"})
 public class DataInitializer implements ApplicationRunner {
 
   private final JpaTutorRepository tutorRepository;
@@ -41,30 +43,58 @@ public class DataInitializer implements ApplicationRunner {
 
     tutorRepository.save(
         new Tutor(
-            tutorId, "아무개", "서울시 용산구", Curriculum.HIGH, new AcademicInfo("아무대학교", "아무학과", "3학년")));
+            tutorId,
+            "아무개",
+            "서울시 용산구",
+            Curriculum.HIGH,
+            new AcademicInfo("아무대학교", "아무학과", "3학년"),
+            new Coordinate(123.123, 123.123)));
     tuteeRepository.save(
-        new Tutee(tuteeId, "튜티아무개", "서울시 용산구", new AcademicInfo("아무고등학교", "인문계", "3학년")));
+        new Tutee(
+            tuteeId,
+            "튜티아무개",
+            "서울시 용산구",
+            new AcademicInfo("아무고등학교", "인문계", "3학년"),
+            new Coordinate(123.123, 123.123)));
 
     tutoringRepository.save(
         Tutoring.builder().tutorId(tutorId).tuteeId(tuteeId).tutoringName("수학을 정복하자!").build());
     saveTutorsAndTuteesForPaging();
   }
 
-  public void saveTutorsAndTuteesForPaging(){
+  public void saveTutorsAndTuteesForPaging() throws ParseException {
     String tutorId = "tutor";
     String tuteeId = "tutee";
-    for(int i = 0; i < 10; i++){
-      accountRepository.save(new Account(tutorId + i, passwordEncoder.encode(tutorId + i),
-          "010-1234-56780", RoleType.ROLE_TUTOR.name()));
-      accountRepository.save(new Account(tuteeId + i, passwordEncoder.encode(tuteeId + i),
-          "010-1234-56780", RoleType.ROLE_TUTOR.name()));
+    for (int i = 0; i < 10; i++) {
+      accountRepository.save(
+          new Account(
+              tutorId + i,
+              passwordEncoder.encode(tutorId + i),
+              "010-1234-56780",
+              RoleType.ROLE_TUTOR.name()));
+      accountRepository.save(
+          new Account(
+              tuteeId + i,
+              passwordEncoder.encode(tuteeId + i),
+              "010-1234-56780",
+              RoleType.ROLE_TUTOR.name()));
     }
-    for(int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++) {
       tutorRepository.save(
           new Tutor(
-              tutorId + i, "아무개" + i, "서울시 용산구", Curriculum.HIGH, new AcademicInfo("아무대학교", "아무학과", "3학년")));
+              tutorId + i,
+              "아무개" + i,
+              "서울시 용산구",
+              Curriculum.HIGH,
+              new AcademicInfo("아무대학교", "아무학과", "3학년"),
+              new Coordinate(123.123, 123.123)));
       tuteeRepository.save(
-          new Tutee(tuteeId + i, "튜티아무개" + i, "서울시 용산구", new AcademicInfo("아무고등학교", "인문계", "3학년")));
+          new Tutee(
+              tuteeId + i,
+              "튜티아무개" + i,
+              "서울시 용산구",
+              new AcademicInfo("아무고등학교", "인문계", "3학년"),
+              new Coordinate(123.123, 123.123)));
     }
   }
 }
