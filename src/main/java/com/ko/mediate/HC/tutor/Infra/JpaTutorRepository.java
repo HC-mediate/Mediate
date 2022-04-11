393c74d40283cf4eb74b46b638b7d2f2a999f939
arsgsg1 <arsgsg1@gmail.com>
2022-04-11 15:23:47 +0900

    레포지토리에 Profile 어노테이션 제거

--- src/main/java/com/ko/mediate/HC/tutor/Infra/JpaTutorRepository.java
+++ src/main/java/com/ko/mediate/HC/tutor/Infra/JpaTutorRepository.java
@@ -3,13 +3,11 @@ package com.ko.mediate.HC.tutor.Infra;
 import com.ko.mediate.HC.tutor.domain.Tutor;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Profile(value = {"local-maria", "prod1", "prod2"})
 @Repository
 public interface JpaTutorRepository extends JpaRepository<Tutor, Long>, JpaTutorCustomRepository {
   @Query("SELECT t FROM Tutor t WHERE t.accountId.accountId = :accountId")
