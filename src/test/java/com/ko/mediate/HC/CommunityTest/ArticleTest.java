412f358633c1141cb53ab59d8e55aa592b2bdc26
arsgsg1 <arsgsg1@gmail.com>
2022-05-16 22:30:34 +0900

    커뮤니티 글 테스트 url 수정

--- src/test/java/com/ko/mediate/HC/CommunityTest/ArticleTest.java
+++ src/test/java/com/ko/mediate/HC/CommunityTest/ArticleTest.java
@@ -5,6 +5,7 @@ import com.ko.mediate.HC.common.infra.S3Uploader;
 import com.ko.mediate.HC.community.application.dto.request.RequestArticleDto;
 import com.ko.mediate.HC.community.domain.Article;
 import com.ko.mediate.HC.community.domain.ArticleType;
+import com.ko.mediate.HC.community.domain.Category;
 import com.ko.mediate.HC.community.infra.JpaArticleRepository;
 import io.findify.s3mock.S3Mock;
 import java.io.IOException;
@@ -30,6 +31,7 @@ public class ArticleTest extends HcApplicationTests {
   @Autowired private S3Uploader s3Uploader;
   @Autowired private S3Mock s3Mock;
   @Autowired private MockMvc mvc;
+  private final String baseUrl = "/api/articles";
 
   @Test
   @DisplayName("파일 업로드 테스트 With S3 Mock")
@@ -45,12 +47,12 @@ public class ArticleTest extends HcApplicationTests {
   @Test
   @DisplayName("글 생성 테스트")
   public void createArticleTest() throws Exception {
-    RequestArticleDto dto = new RequestArticleDto("title", "content", ArticleType.QUESTION);
+    RequestArticleDto dto = new RequestArticleDto("title", "content", ArticleType.QUESTION, Category.STUDY_QUESTION);
     MockMultipartFile mockMultipartFile = new MockMultipartFile("imgFile", "cat.jpg", MediaType.IMAGE_JPEG_VALUE, "test data".getBytes());
     MockMultipartFile dtoMutlipartFile = new MockMultipartFile("dto", null, MediaType.APPLICATION_JSON_VALUE, objectMapper.writeValueAsString(dto).getBytes(
         StandardCharsets.UTF_8));
 
-    mvc.perform((multipart("/api/article")
+    mvc.perform((multipart(baseUrl)
         .file(mockMultipartFile).part(new MockPart("imgFile", mockMultipartFile.getBytes()))
         .file(dtoMutlipartFile).part(new MockPart("dto", dtoMutlipartFile.getBytes()))
             .header("Authorization", BEARER + token)
