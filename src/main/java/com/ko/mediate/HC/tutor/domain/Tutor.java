5632b465f4aa2fed86c248017bb2e5eb4496b563
arsgsg1 <arsgsg1@gmail.com>
2022-05-31 20:58:11 +0900

    누락된 채번 전략 코드 추가

--- src/main/java/com/ko/mediate/HC/tutor/domain/Tutor.java
+++ src/main/java/com/ko/mediate/HC/tutor/domain/Tutor.java
@@ -12,6 +12,7 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -28,7 +29,9 @@ import org.locationtech.jts.geom.Point;
 @Getter
 @Table(name = "tb_tutor")
 public class Tutor {
-  @Id @GeneratedValue private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "account_id")
