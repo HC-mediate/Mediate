d52724013c25627db9c9124ff1e16a7285b6e000
jhyun <arsgsg1@gmail.com>
2022-09-20 16:25:20 +0900

    인덴트 4 space로 수정 및 코드 포맷팅

--- src/test/java/com/ko/mediate/HC/config/LocalRedisConfig.java
+++ src/test/java/com/ko/mediate/HC/config/LocalRedisConfig.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+
 import org.junit.jupiter.api.Nested;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -19,98 +20,105 @@ import redis.embedded.RedisServer;
 
 @TestConfiguration
 public class LocalRedisConfig {
-  private int redisPort = 6378;
-  private RedisServer redisServer;
+    private int redisPort = 6378;
+    private RedisServer redisServer;
 
-  @PostConstruct
-  public void redisServer() throws IOException {
-    int port = isRedisRunning() ? findAvailablePort() : redisPort;
-    if(isArmMac()){
-      redisServer = new RedisServer(Objects.requireNonNull(getRedisFileForArcMac()),
-              redisPort);
-    }
-    else{
-      redisServer = new RedisServer(port);
-    }
-    redisServer.start();
-  }
-
-  private boolean isArmMac() {
-    return Objects.equals(System.getProperty("os.arch"), "aarch64") &&
-            Objects.equals(System.getProperty("os.name"), "Mac OS X");
-  }
-
-  private File getRedisFileForArcMac() throws IOException {
-    try {
-      return new ClassPathResource("binary/redis/redis-server-6.2.5-mac-arm64").getFile();
-    } catch (Exception e) {
-      throw new IOException();
-    }
-  }
-
-  @Nested
-  class LocalTestRedisClientConfig {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-      return new LettuceConnectionFactory("127.0.0.1", redisPort);
-    }
-  }
-
-  @PreDestroy
-  public void stopRedis() {
-    if (redisServer != null) {
-      redisServer.stop();
-    }
-  }
-
-  /** Embedded Redis가 현재 실행중인지 확인 */
-  private boolean isRedisRunning() throws IOException {
-    return isRunning(executeGrepProcessCommand(redisPort));
-  }
-
-  /** 현재 PC/서버에서 사용가능한 포트 조회 */
-  public int findAvailablePort() throws IOException {
-
-    for (int port = 10000; port <= 65535; port++) {
-      Process process = executeGrepProcessCommand(port);
-      if (!isRunning(process)) {
-        return port;
-      }
+    @PostConstruct
+    public void redisServer() throws IOException {
+        int port = isRedisRunning() ? findAvailablePort() : redisPort;
+        if (isArmMac()) {
+            redisServer = new RedisServer(Objects.requireNonNull(getRedisFileForArcMac()),
+                    redisPort);
+        } else {
+            redisServer = new RedisServer(port);
+        }
+        redisServer.start();
     }
 
-    throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
-  }
-
-  /** 해당 port를 사용중인 프로세스 확인하는 sh 실행 */
-  private Process executeGrepProcessCommand(int port) throws IOException {
-    String command;
-    String[] shell;
-    String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT); // 현재 VM이 실행 중인 OS 이름 얻기
-    if (osName.indexOf("nix") >= 0 || osName.indexOf("nux") >= 0 || osName.indexOf("aix") > 0 || osName.startsWith("mac")) {
-      command = String.format("netstat -nat | grep LISTEN|grep %d", port);
-      shell = new String[] {"/bin/sh", "-c", command};
-    } else {
-      command = String.format("netstat -nao | find \"LISTEN\" | find \"%d\"", port);
-      shell = new String[] {"cmd.exe", "/y", "/c", command};
-    }
-    return Runtime.getRuntime().exec(shell);
-  }
-
-  /** 해당 Process가 현재 실행중인지 확인 */
-  private boolean isRunning(Process process) {
-    String line;
-    StringBuilder pidInfo = new StringBuilder();
-
-    try (BufferedReader input =
-        new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-
-      while ((line = input.readLine()) != null) {
-        pidInfo.append(line);
-      }
-
-    } catch (Exception e) {
+    private boolean isArmMac() {
+        return Objects.equals(System.getProperty("os.arch"), "aarch64") &&
+                Objects.equals(System.getProperty("os.name"), "Mac OS X");
     }
 
-    return StringUtils.hasText(pidInfo.toString());
-  }
+    private File getRedisFileForArcMac() throws IOException {
+        try {
+            return new ClassPathResource("binary/redis/redis-server-6.2.5-mac-arm64").getFile();
+        } catch (Exception e) {
+            throw new IOException();
+        }
+    }
+
+    @Nested
+    class LocalTestRedisClientConfig {
+        @Bean
+        public RedisConnectionFactory redisConnectionFactory() {
+            return new LettuceConnectionFactory("127.0.0.1", redisPort);
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+    /**
+     * Embedded Redis가 현재 실행중인지 확인
+     */
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 현재 PC/서버에서 사용가능한 포트 조회
+     */
+    public int findAvailablePort() throws IOException {
+
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    /**
+     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
+     */
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command;
+        String[] shell;
+        String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT); // 현재 VM이 실행 중인 OS 이름 얻기
+        if (osName.indexOf("nix") >= 0 || osName.indexOf("nux") >= 0 || osName.indexOf("aix") > 0 || osName.startsWith("mac")) {
+            command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+            shell = new String[]{"/bin/sh", "-c", command};
+        } else {
+            command = String.format("netstat -nao | find \"LISTEN\" | find \"%d\"", port);
+            shell = new String[]{"cmd.exe", "/y", "/c", command};
+        }
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    /**
+     * 해당 Process가 현재 실행중인지 확인
+     */
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input =
+                     new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+
+        } catch (Exception e) {
+        }
+
+        return StringUtils.hasText(pidInfo.toString());
+    }
 }
