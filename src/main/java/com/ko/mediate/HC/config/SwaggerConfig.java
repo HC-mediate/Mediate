f6c61868167790524be1628631570909e8b5a355
jhyun <arsgsg1@gmail.com>
2022-09-27 21:31:01 +0900

    [feat-15] 스웨거 버전 3로 업데이트
    1. spring 2.6.x 버전과 호환되지 않아 업데이트합니다.

--- src/main/java/com/ko/mediate/HC/config/SwaggerConfig.java
+++ src/main/java/com/ko/mediate/HC/config/SwaggerConfig.java
@@ -4,43 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 
-@Configuration
-@EnableSwagger2
 @EnableWebMvc
 @Profile({"local", "prod1", "prod2", "local-maria"})
-public class SwaggerConfig implements WebMvcConfigurer {
+@Configuration
+public class SwaggerConfig {
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.ant("/api/**"))
+                .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(Arrays.asList(apiKey()));
-
+                .apiInfo(apiInfo());
     }
 
-    private ApiKey apiKey() {
-        return new ApiKey("Bearer +accessToken", "Authorization", "header");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Mediate Swagger")
+                .description("Mediate API에 대한 스웨거 문서입니다.")
+                .version("1.0")
+                .build();
     }
 }
