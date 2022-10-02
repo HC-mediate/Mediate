147db9677c285df5e7499e65eb3d96d5c6588e57
jhyun <arsgsg1@gmail.com>
2022-10-02 17:49:08 +0900

    [feat-15] #15 ModelAttribute 객체에 바인딩하기 위한 @Setter 추가

--- src/main/java/com/ko/mediate/HC/community/application/dto/request/CreateArticleDto.java
+++ src/main/java/com/ko/mediate/HC/community/application/dto/request/CreateArticleDto.java
@@ -7,15 +7,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateArticleDto {
+    @NotBlank(message = "글의 제목은 반드시 있어야 합니다.")
     private String title;
     private String content;
     private Category category;
